f93e2ee4826a619708fd56fa231d922d9ec6dbd7
mewest <mewest@gmail.com>
2010-02-24 02:29:54 +0000

    Update to whichgismo paths

--- GISMO/contributed_internal/gismo_management/whichgismo_paths.m
+++ GISMO/contributed_internal/gismo_management/whichgismo_paths.m
@@ -8,10 +8,9 @@
 
 % PREDEFINED PATH
 paths = {
-    'LinuxNet'       '/home/admin/share/matlab/PACKAGES/GISMO_OP/GISMO'
-    'LinuxNetDev'    '/home/admin/share/matlab/PACKAGES/GISMO_DEV/GISMO'
-    'LinuxNetWest'    '/home/west/mlcode/GISMO'
-    'LinuxNetOld'    '/home/admin/share/matlab/PACKAGES/GISMO'
+    'LinuxNet'          '/home/admin/share/matlab/PACKAGES/GISMO_OP/GISMO'
+    'LinuxNetDev'       '/home/admin/share/matlab/PACKAGES/GISMO_DEV/GISMO'
+    'LinuxNetWest'      '/home/west/mlcode/GISMO_WEST/GISMO'
     'WestPCDev'         'C:\Users\Michael West\Documents\mlcode\GISMO_DEV\GISMO'
     'WestPCBranch'      'C:\Users\Michael West\Documents\mlcode\GISMO_BRANCH_MEW\GISMO'
 };
