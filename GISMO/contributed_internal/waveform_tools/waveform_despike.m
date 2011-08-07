4bf3dfc12808e46b8dc14960f58c174678c65b43
gthompson <gthompson@alaska.edu>
2011-08-07 21:07:34 +0000

    waveform_despike now works with a matrix of waveform objects

--- GISMO/contributed_internal/waveform_tools/waveform_despike.m
+++ GISMO/contributed_internal/waveform_tools/waveform_despike.m
@@ -1,8 +1,11 @@
 function [w] = waveform_despike(w);
 % [w] = waveform_despike(w);
 % Glenn Thompson, 2009
-
-y = get(w, 'data'); 
-[y, ip] = despike( y );
-set(w, 'data', y);
+[m, n] = size(w);
+for i = 1 : numel(w)
+  y = get(w(i), 'data'); 
+  [y, ip] = despike( y );
+  w(i) = set(w(i), 'data', y);
+end
+w = reshape(w, m, n);
 
