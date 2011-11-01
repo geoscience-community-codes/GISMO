6edfd08586f874d0e81dfc5c5c5ed17dacad539f
gthompson <gthompson@alaska.edu>
2011-11-01 05:41:35 +0000

    added help comments to dbtable_present

--- GISMO/contributed/catalog/+libgt/dbtable_present.m
+++ GISMO/contributed/catalog/+libgt/dbtable_present.m
@@ -1,4 +1,11 @@
 function present=dbtable_present(dbpath, table)
+% DBTABLE_PRESENT Check if a Datascope database table exists and has more than 0 rows. 
+%
+%    PRESENT = DBTABLE_PRESENT(DBPATH, TABLE)
+
+% AUTHOR: Glenn Thompson, UAF-GI
+% $Date:$
+% $Revision:$
 present = 0;
 libgt.print_debug(sprintf('%s: checking %s.%s has > 0 rows\n',mfilename,dbpath,table),2);
 if exist(dbpath, 'file')
