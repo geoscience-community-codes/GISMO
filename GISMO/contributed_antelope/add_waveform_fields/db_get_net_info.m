4149c3929bf1d0929b9740423bf67eb6685eaf11
carltape <carltape@b512c968-07be-11df-bdae-5552c88b53c9>
2012-08-09 00:11:19 +0000

    updates to db_get_net_info.m to handle missing networks or more than one network listed

--- GISMO/contributed_antelope/add_waveform_fields/db_get_net_info.m
+++ GISMO/contributed_antelope/add_waveform_fields/db_get_net_info.m
@@ -1,13 +1,12 @@
 function net = db_get_net_info(stalist,dbname)
 
-
 % NET = DB_GET_NET_INFO(STATION_LIST,DATABASE_NAME) 
 % This function takes a list of stations and returns a structure
 % containing station and network names from the snetsta table. 
 % The database must include a "snetsta" table.
 %
 % Example:
-%    net = db_get_net_info({'AUL' 'AUH' 'AUW'},'dbtmp')
+%    net = db_get_net_info({'AUL' 'AUH' 'AUW'},'/aerun/sum/params/Stations/master_stations')
 % 
 % 
 % Yun Wang 03/25/2012
@@ -24,9 +23,20 @@ net = repmat(cell(1),nr,1);
 db = dbopen(dbname,'r');
 db = dblookup_table(db,'snetsta');
 for n = 1:nr;
-	db1 = dbsubset(db,['sta==''' sta{n} '''']);
+        db1 = dbsubset(db,['sta==''' sta{n} '''']);
     recnum = dbquery(db1,'dbRECORD_COUNT');
-	net(n) = {dbgetv(db1,'snet')};
+    if recnum == 0
+       disp(sprintf('Station %s is missing in "snetsta" table', sta{n})); 
+       net(n) = {'NULL'};
+    elseif recnum ==1
+       net(n) = {dbgetv(db1,'snet')};
+    else
+       disp(sprintf('Station %s has more than 1 record found in "snetsta" table, use the first record',sta{n}));
+       nettemp = dbgetv(db1,'snet');
+       net(n)=nettemp(1);
+    end    
+
 end
 dbclose(db);
 end
+
