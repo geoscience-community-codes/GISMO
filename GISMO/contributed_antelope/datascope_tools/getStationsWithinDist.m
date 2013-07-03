26f2c8b5d48de65a6016819c13021717df29d43f
gthompson <gthompson@alaska.edu>
2013-07-03 22:26:30 +0000

    added new arguments to enable get stations for old time periods

--- GISMO/contributed_antelope/datascope_tools/getStationsWithinDist.m
+++ GISMO/contributed_antelope/datascope_tools/getStationsWithinDist.m
@@ -1,6 +1,6 @@
-function station = getStationsWithinDist(lon, lat, distkm, dbstations, maxsta)
+function station = getStationsWithinDist(lon, lat, distkm, dbstations, maxsta, snum, enum)
 %GETSTATIONSWITHINDIST 
-% station = getStationsWithinDist(lon, lat, distkm, dbstations)
+% station = getStationsWithinDist(lon, lat, distkm, dbstations, maxsta, snum, enum)
 % load station/channels within DISTKM km of a given (LON, LAT) 
 % from site database DBSTATIONS
 % 
@@ -15,6 +15,10 @@ function station = getStationsWithinDist(lon, lat, distkm, dbstations, maxsta)
 %	latitude	- station latitude
 %	elev	- station elevation
 %	distance	- distance (in km) from input (LON, LAT)
+%
+% Example: Get stations within 100km of (-150.0, 60.5) that were
+% operational in 1990:
+% s = getStationsWithinDist(-150.0, 60.5, 100.0, '/avort/oprun/dbmaster/master_stations', 10, datenum(1990,1,1), datenum(1991,1,1));
 
 % AUTHOR: Glenn Thompson, UAF-GI
 % $Date: $
@@ -23,14 +27,49 @@ if ~exist('maxsta', 'var')
 	maxsta = 999;
 end
 db = dbopen(dbstations, 'r');
+
+% Filter the site table
 db = dblookup_table(db, 'site');
 db = dbsubset(db, sprintf('distance(lon, lat, %.4f, %.4f)<%.4f',lon,lat,km2deg(distkm)));
-db = dbsubset(db, sprintf('offdate == NULL'));
+if ~exist('snum', 'var')
+    % No start time given, so assume we just want sites that exist today.
+    % Remove any sites that have been decommissioned
+    db = dbsubset(db, sprintf('offdate == NULL'));
+else
+    % Remove any sites that were decommissioned before the start time
+    db = dbsubset(db, sprintf('offdate == NULL || offdate > %s',datenum2julday(snum)));
+end
+% Remove any sites that were installed after the end time (this may remove
+% some sites that exist today)
+if exist('enum', 'var')
+    db = dbsubset(db, sprintf('ondate  < %s',datenum2julday(enum)));
+end
+
+% Filter the sitechan table
 db2 = dblookup_table(db, 'sitechan');
-db2 = dbsubset(db2, '(chan=~/[BES]H[ENZ]/  || chan=~/BD[FL]/) && offdate == NULL');
+db2 = dbsubset(db2, 'chan=~/[BES]H[ENZ]/  || chan=~/BD[FL]/');
+if ~exist('snum', 'var')
+    % No start time given, so assume we just want sites that exist today.
+    % Remove any sites that have been decommissioned
+    db2 = dbsubset(db2, sprintf('offdate == NULL'));
+else
+    % Remove any sites that were decommissioned before the start time
+    db2 = dbsubset(db2, sprintf('offdate == NULL || offdate > %s',datenum2julday(snum)));
+end
+% Remove any sites that were installed after the end time (this may remove
+% some sites that exist today)
+if exist('enum', 'var')
+    db2 = dbsubset(db2, sprintf('ondate  < %s',datenum2julday(enum)));
+end
+
+% Join site and sitechan
 db2 = dbjoin(db, db2);
+
+% Join to snetsta
 db3 = dblookup_table(db, 'snetsta');
 db3 = dbjoin(db2, db3);
+
+% Read data
 net = dbgetv(db3, 'snet');
 if ~iscell(net)
 	net = {net};
@@ -74,4 +113,5 @@ end
 % limit the number of stations
 numstations = min([maxsta numel(station)]);
 station = station(1:numstations);
+
 
