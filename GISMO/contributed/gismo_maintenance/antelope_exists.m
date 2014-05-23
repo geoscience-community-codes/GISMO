1ad9d3ac339736e2e9c3dffcc80181680a02035b
mewest <mewest@alaska.edu>
2014-05-23 05:11:41 +0000

    fixed erroneous change in antelope_exists

--- GISMO/contributed/gismo_maintenance/antelope_exists.m
+++ GISMO/contributed/gismo_maintenance/antelope_exists.m
@@ -2,17 +2,18 @@ function TF = antelope_exists()
 
 %ANTELOPE_EXISTS check for Antelope Toolbox for Matlab
 %  TF = ANTELOPE_EXISTS returns 1 if the Antelope Toolbox for Matlab is
-%  present in the path. This function does not check to see whether the
-%  Antelope Toolbox is functioning correctly. It merely searches the path
-%  for functions. The Antelope Toolbox for Matlab is maintained by Kent
-%  Lindquist of Lindquist Consulting.
+%  present in the Matlab path. This function does not check to see whether
+%  the Antelope Toolbox is functioning correctly. It merely searches the
+%  path for the existence of certain functions. To test the actual validity
+%  of the Antelope toolbox you might try the example suite executed by
+%  dbexample_runall (an example in the Antelope Toolbox for Matlab). The
+%  Antelope Toolbox for Matlab is not part of GISMO though GISMO includes
+%  many utilities built on the toolbox. It is maintained by Lindquist
+%  Consulting, Inc.
 
 % Author: Michael West, Geophysical Institute, Univ. of Alaska Fairbanks
 % $Date$
 % $Revision$
-
-
-admin.deprecated(mfilename,'admin.antelope_exists');
 
 
 
