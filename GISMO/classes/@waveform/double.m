7033602d72e7603b113f6bcef4865a998b7a08dd
Celso <celsoborealis@gmail.com>
2015-09-11 19:00:16 +0200

    excellent rewrite of double.m
    I had convoluted code which I cleaned up. I also made it more versatile.
    * Added ability to pad with any function that creates an array using
    the form N = fn(Rows, Cols).  Example existing functions include
    ‘ones’, ‘zeros’, ‘nan’
    * added ability to use function handles instead of function names,
    which might be a tiny bit faster.
    * removed restriction on 1xN or Nx1 waveforms.  They can now be any
    shape, but will be returned as MxN
    * got rid of an egregious eval.

--- GISMO/classes/@waveform/double.m
+++ GISMO/classes/@waveform/double.m
@@ -7,24 +7,25 @@ function n = double(W, option)
 %   vectors.  Using the OPTION, you can specify a NaN padding instead.
 %
 %   Input Arguments
-%       WAVEFORM: a waveform object   1xN DIMENSIONAL
-%       OPTION: optional, the string 'NaN'.  If specified, padding will be
-%               with NaN; Otherwise, data is zero padded on the end.
+%       WAVEFORM: a waveform object   assumed as 1xN DIMENSIONAL 
+%               (If not, it will be treated as though it were 1xN!)
+%       OPTION: Handle to any function that creates an array of a 
+%               dictated size.  The chosen function should work like:
+%               A = (nRows,nCols); and return a double array of the
+%               appropriate size. ex. @ones, @zeros, @nan.
 %
 %   Output Arguments
 %       N: the columns of data
 %
 %   example:
-%       % W(1) is a waveform object with data [0 1 2 5 3]
-%       % W(2) is a waveform object with data [2 5]
+%       % let W(1) be a waveform with data [0 1 2 5 3]
+%       % let W(2) be a waveform with data [2 5]
 %
 %       Y = double(W);
 %       % Y is now [[ 0 2; 1 5; 2 0; 5 0; 3 0]
 %
-%       Z = double(W,'NaN'); pads with NaN instead of zeros
+%       Z = double(W,@nan); pads with NaN instead of zeros
 %       % Z is now [ 0 2; 1 5; 2 NaN; 5 NaN; 3 NaN]
-%
-%
 %
 % See also WAVEFORM/GET, NAN
 
@@ -32,41 +33,36 @@ function n = double(W, option)
 % $Date$
 % $Revision$
 
-if ~exist('option','var')
-    option = 'zeros';
+if numel(W) <= 1
+   n = W.data; %  assumption --> data is already a column of double!
+   return
+end
+
+if nargin == 1
+   createDefaultArray = @zeros;
 else
-    option = lower(option);
-    switch option
-        case 'nan' %do nothing, we're set...
-        otherwise
-            option = 'zeros';
-    end
-end
-%  force dimensionality
-
-if numel(W) ~= length(W) 
-  error('Waveform:double:invalidWaveformDimensions',...
-    'Waveforms must be 1 x N or N x 1');
+   if ischar(option) 
+      createDefaultArray = str2func(option);
+   elseif isa(option,'function_handle')
+      createDefaultArray = option;
+   else
+      error('waveform:double:invalidOption',...
+         '''option'' must be either a function handle or function name.');
+   end
 end
 
-% find out how many elements in each waveform
 m = get(W,'data_length');
-%emptywaves = find(m==0);
-try
-    if sum(m==0)~=0 % this is not a typo
-        error('Waveform:double:forceCatch',...
-          'Forcing move to catch'); %force us to the catch section...
-    end
-    % simplest way of doing this
-    
-        n = [W(:).data];
-catch %craps out if not all data lengths the same
+if isempty(m)
+   n = [];
+   return
+end
 
-    %create array to match outgoing datafile
-    eval(['n = ', option, '(max(m),numel(W));']); %akin to n=zeros(max(m,x))
-
-    % fill with the actual data, end result is zero or nan padded
-    for x=1:numel(W)
-        n(1:m(x),x) = get(W(x),'data');
-    end
+m=m(:);
+if all(m == m(1))
+   n = [W(:).data]; % easy-peazy since all data is of same length.
+else
+   n = createDefaultArray(max(m),numel(W));
+   for x=1:numel(W)
+      n(1:m(x),x) = W(x).data; % assumes that data is a column of double!
+   end
 end
