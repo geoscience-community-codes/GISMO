f239489a32558496e8783d19adad1e3911ea1a91
Celso <celsoborealis@gmail.com>
2015-09-14 10:39:48 +0200

    added skeleton for datasource replacement
    this stuff will probably change.

---
+++ GISMO/classes/+dataretrieval/sacsource.m
@@ -0,0 +1,15 @@
+classdef sacsource < spatiotemporal_file
+   %UNTITLED2 Summary of this class goes here
+   %   Detailed explanation goes here
+   
+   properties
+   end
+   
+   methods
+      function data = retrieve(obj, where_, from_ , until_)
+         error('unimplemented SAC source');
+      end
+      
+   end
+   
+end
---
+++ GISMO/classes/+dataretrieval/spatiotemporal_datasource.m
@@ -0,0 +1,16 @@
+classdef spatiotemporal_datasource
+   %UNTITLED2 Summary of this class goes here
+   %   Detailed explanation goes here
+   
+   properties
+   end
+   
+   methods
+      function data = retrieve(obj, where_, from_ , until_)
+         error('unimplemented base datasource class');
+      end
+      
+   end
+   
+end
+
