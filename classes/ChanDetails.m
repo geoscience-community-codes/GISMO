3247d3f1558729996cc4b70534e882e714f8fa18
Celso <celsoborealis@gmail.com>
2015-10-21 09:11:18 +0200

    added basic ChanDetails class
    which retrieves data from the IRIS FDSN station service at the network
    level.  You can use arbitrary search parameters as described by the
    service.  Results are in a 1xN array of ChanDetails which can each be
    treated like a struct.  to find a particular station of interest use
    .getbyname('net.sta.loc.cha')

--- classes/ChanDetails.m
+++ classes/ChanDetails.m
@@ -1,12 +1,34 @@
 classdef ChanDetails
-   %UNTITLED Summary of this class goes here
-   %   Detailed explanation goes here
+   %ChanDetails Summary of this class goes here
+   %   Access fields as though this were a struct 
+   %
+   %  Request data using
+   %  ChanDetails.retrieve([], ...) retrieve data from default source:
+   %    https://service.iris.edu/fdsnws/station/1/
+   %  What follows are parameter-value pairs that filter which data is
+   %  retrieved.  These are described in detail from the iris website
+   %   <a href>https://service.iris.edu/fdsnws/station/1/</a>
+   %  
+   %  Examples:
+   %    Ex 1. Retrieve all stations that start with A, for network IU. But
+   %    only the BH channels (excluding BHZ)
+   %    ChanDetails.retrieve([],'station','A*','network','IU','channel','BH?,-BHZ')
+   %
+   %    Results are in an 1XN ChanDetails object
+   
+   properties(Dependent)
+      name
+      network
+      station
+      location
+      channel
+   end
    
    properties
-      channeltag
-      lat
-      lon
-      elev
+      channelinfo
+      latitude
+      longitude
+      elevation
       depth
       azimuth
       dip
@@ -16,15 +38,48 @@ classdef ChanDetails
       scaleunits
       samplerate
       starttime
-      endttime
-   end
-   properties(Dependent)
-      network
-      station
-      location
-      channel
+      endtime
    end
    methods
+           %% get/set of channel-related data
+      function N = get.network(obj)
+         N = obj.channelinfo.network;
+      end
+      function obj = set.network(obj, val)
+         obj.channelinfo.network = val;
+      end
+      
+      function S = get.station(obj)
+         S = obj.channelinfo.station;
+      end
+      function obj = set.station(obj, val)
+         obj.channelinfo.station = val;
+      end
+      
+      function L = get.location(obj)
+         L = obj.channelinfo.location;
+      end
+      function obj = set.location(obj, val)
+         obj.channelinfo.location = val;
+      end
+      
+      function C = get.channel(obj)
+         C = obj.channelinfo.channel;
+      end
+      function obj = set.channel(obj, val)
+         obj.channelinfo.channel = val;
+      end
+      
+      function S = get.name(obj)
+         S = obj.channelinfo.string;
+      end
+      function C = getbyname(obj,val)
+         % getbyname does not know wildcards!
+         % retrieves values that match requeste Net.Sta.Loc.Cha name
+         % ex.
+         % chandeets.getbyname('IU.ANMO.00.BH1')
+         C = obj(strcmp({obj.name},val));
+      end
    end
    methods(Static)
       function chdeets = retrieve(ds,varargin)
@@ -96,7 +151,39 @@ classdef ChanDetails
            urlstr = [urlstr, '&', name, '=', val];
         end
         chdeets = urlread(urlstr);
-        % parse A
+        labels = textscan(chdeets,'%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s',1,'delimiter','|');
+        labels=[labels{:}];
+        labels(1) = {labels{1}(2:end)}; %first label starts with #;
+        labels = lower(strtrim(labels));
+        C = textscan(chdeets,'%s %s %s %s %f %f %f %f %f %f %s %f %f %s %f %s %s','delimiter','|','commentstyle','#');
+
+        % convert station network channel location to channeltags and clean
+        chanTags = channeltag(strcat(...
+           C{strcmp(labels,'network')},'.',...
+           C{strcmp(labels,'station')},'.',...
+           C{strcmp(labels,'location')},'.',...
+           C{strcmp(labels,'channel')}));
+        C{1} = chanTags; C(2:4) = [];
+        labels{1} = 'channelinfo'; labels(2:4)=[];
+        % clean up time fields, and convert
+        timefields = find(ismember(labels,{'starttime','endtime'}));
+        for n=timefields
+           C{n} = datenum(strrep(C{n},'T',' '));
+        end
+        % parse it
+        clear chdeets
+        for n=1:numel(chanTags)
+           chdeets(n) = ChanDetails;
+           for x = 1 : numel(labels);
+              fn = labels{x};
+              if iscell(C{x})
+                 thisval = C{x}{n};
+              else
+                 thisval = C{x}(n);
+              end
+              chdeets(n).(fn) = thisval;
+           end
+        end
       end
    end
    
