8d1600004d85a5cd093bff2f990afc6f6061b7c4
Glenn Thompson <thompsong@usf.edu>
2017-03-22 14:18:44 -0400

    updated cookbooks

--- publish_cookbooks.m
+++ publish_cookbooks.m
@@ -1,60 +1,59 @@
-function publish_cookbooks()
-    global gismopath
 
-    %% Catalog_cookbook
-    close all
-    mfile = 'Catalog_cookbook';
-    outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
-    mkdir(outdir);
-    try
-        htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
-    end
+global gismopath TESTDATA
 
-    %% correlation_cookbook
-    close all
-    mfile = 'correlation_cookbook';
-    outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
-    mkdir(outdir);
-    try
-        htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
-    end
-
-    %% drumplot_cookbook
-    close all
-    mfile = 'drumplot_cookbook';
-    outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
-    mkdir(outdir);
-    try
-        htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
-    end 
-    
-    %% EventRate_cookbook
-    close all
-    mfile = 'EventRate_cookbook';
-    outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
-    mkdir(outdir);
-    try
-        htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
-    end 
-
-    %% rsam_cookbook
-    close all
-    mfile = 'rsam_cookbook';
-    outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
-    mkdir(outdir);
-    try
-        htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
-    end 
-
-    %% waveform_cookbook
-    close all
-    mfile = 'waveform_cookbook';
-    outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
-    mkdir(outdir);
-    try
-        htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
-    end 
-
+%% Catalog_cookbook
+close all
+mfile = 'Catalog_cookbook';
+outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
+mkdir(outdir);
+try
+    htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
 end
-%%
+
+%% correlation_cookbook
+close all
+mfile = 'correlation_cookbook';
+outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
+mkdir(outdir);
+try
+    htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
+end
+
+%% drumplot_cookbook
+close all
+mfile = 'drumplot_cookbook';
+outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
+mkdir(outdir);
+try
+    htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
+end 
+
+%% EventRate_cookbook
+close all
+mfile = 'EventRate_cookbook';
+outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
+mkdir(outdir);
+try
+    htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
+end 
+
+%% rsam_cookbook
+close all
+mfile = 'rsam_cookbook';
+outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
+mkdir(outdir);
+try
+    htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
+end 
+
+%% waveform_cookbook
+close all
+mfile = 'waveform_cookbook';
+outdir = fullfile(gismopath, '..', 'GISMO.website', 'cookbook_results');
+mkdir(outdir);
+try
+    htmlfile = publish(mfile, 'format', 'html', 'outputDir', outdir, 'createThumbnail', true)
+end 
+
+
 
