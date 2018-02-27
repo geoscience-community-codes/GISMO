89476cd393a1b47dc282ff735584472da19e1d90
Glenn Thompson <thompsong@mail.usf.edu>
2018-02-27 14:20:19 -0500

    modified zmap catalog loader after helping Panagiotis. See email

--- core/+Catalog/+read_catalog/zmap.m
+++ core/+Catalog/+read_catalog/zmap.m
@@ -1,12 +1,28 @@
-function self = zmap(zmapdata)
+function self = zmap(fname_or_data)
 %readEvents.zmap Translate a ZMAP-format data matrix into a Catalog object
 % ZMAP format is 10 columns: longitude, latitude, decimal year, month, day,
 % magnitude, depth, hour, minute, second
-    lon = zmapdata(:,1);
-    lat = zmapdata(:,2);
-    time = datenum( floor(zmapdata(:,3)), zmapdata(:,4), zmapdata(:,5), ...
-        zmapdata(:,8), zmapdata(:,9), zmapdata(:,10) );
-    mag = zmapdata(:,6);
-    depth = zmapdata(:,7);
+
+    if ischar(fname_or_data(1))
+        %% read the data
+        data = load(fname_or_data)
+    else
+        data = fname_or_data;
+    end
+    lon = data(:,1);
+    lat = data(:,2);
+    yyyy = data(:,3);
+    mm = data(:,4);
+    dd = data(:,5);
+    hh = data(:,8);
+    mi = data(:,9);
+    if size(data,2) == 10
+        ss = data(:,10);
+    else
+        ss = zeros(size(mi));
+    end
+    time = datenum( floor(yyyy), mm, dd, hh, mi, ss );
+    mag = data(:,6);
+    depth = data(:,7);
     self = Catalog(time, lon, lat, depth, mag, {}, {});
 end
