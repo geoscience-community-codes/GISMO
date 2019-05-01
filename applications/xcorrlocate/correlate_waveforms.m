163eccaf33522e5b16882a20e7adcfb906af8bc7
Mitchell Hastings <mshastings18@gmail.com>
2019-05-01 13:23:45 -0400

    added help doc

--- applications/xcorrlocate/correlate_waveforms.m
+++ applications/xcorrlocate/correlate_waveforms.m
@@ -1,6 +1,22 @@
 function [subset_list, corr_m] = correlate_waveforms(WAVEFORM_OBJECT, CORRTHRESH)
-    % Function to correlate waveforms in a waveform object and keep the
-    % waveforms that correlate above the given threshold to the master waveform
+%CORRELATE_WAVEFORMS correlates all waveforms within the waveform object
+%passed to the function and generate a cell array of the waveforms that
+%correlate above a given threshold on the master waveform (the first on in
+%the object). 
+%       Input Arguments:
+%           WAVEFORM_OBJECT: waveform object containing all the waveforms
+%           in the multiplet family. The master event should be the first
+%           on in the waveform object.
+%           CORRTHRESH: correlation threshold. Recommend using at least
+%           0.7 for multiplet families.
+%       Output/Examples:
+%           subset_list: cell array containing the filenames of all the
+%           waveforms that correlate above the threshold.
+%           corr_m: array of the correlation values between each waveform
+%           and the master waveform.
+%               Ex:
+%                   w = load_waveforms(LIST,TYPE)
+%                   [subset_list,corr_m] = correlate_waveforms(w,0.7)
 
     % create correlation object of the waveforms to do the correlation
     c = correlation(WAVEFORM_OBJECT, get(WAVEFORM_OBJECT, 'start'));
