54e9ff0f175e7ed8852128e275b773dfd45ec863
Glenn Thompson <glennthompson1971@gmail.com>
2023-04-07 16:43:37 -0400

    Update frankelq.m

--- contributed_antelope/attenuation/frankelq.m
+++ contributed_antelope/attenuation/frankelq.m
@@ -1,5 +1,4 @@
 function arrivalobj = frankelq(dbpath, varargin)
-%expr, f1, f2, pretrigger, posttrigger, max_arrivals)
 %FRANKELQ Estimate Q via the Frankel method (1982).
 % FRANKELQ(dbpath) estimate seismic attenuation quality factor, Q, by the 
 % via method described in A. Frankel (1982), "The Effects of Attenuation 
@@ -13,23 +12,41 @@ function arrivalobj = frankelq(dbpath, varargin)
 %   continuous waveform data stored in Miniseed or SAC format. * Dependencies
 %   include Antelope, the Antelope toolbox for MATLAB, and GISMO *.
 %   2. For each arrival time, the corresponding waveform data are loaded,
-%   using a 0.5-s pre-trigger and 1.5-s post-trigger time window (2-s total).
+%   using a 0.3-s pre-trigger and 1.28-s post-trigger time window (1.58s total).
+%   10-seconds before and after this window are also loaded, but discarded later
+%   as part of the processing, which includes:
+%   - Remove spikes of length 1
+%   - Detrend waveform
+%   - Fill gaps by interpolation (sometimes get “NaN” values where there were signal dropouts)
+%   - Apply cosine taper
+%   - Apply two-way (zero-phase) , two-pole high pass filter at 0.5 Hz
+%   - Extract a noise window from the first 10-s (before pre-trigger window)
+%   - Extract a signal window from start of pre-trigger window to end of post-trigger window (cuts off 10-s from each end)
+%   - Optionally integrate signal and noise waveforms from velocity to displacement seismograms (Heather's version was hardwired to use displacement; what did Frankel use? Displacement will be less stable than velocity).
+
+13.	Computes mean spectral amplitudes in lower frequency-band (4-6 Hz by default, i.e. 5 Hz +/- 20%) and upper frequency band (18-22 Hz by default, i.e. 20 Hz +/- 10%) for signal and noise. 
+14.	Compute spectral ratio (low/high)
+
 %   3. For each arrival time, the travel time is also computed, using the
 %       origin time of the seismic event.
-%   4. Spectra (periodograms) are computed for each 2-s waveform.
-%   5. A lower frequency in the range 3.3-7.5 Hz is randomly chosen.
-%   6. A higher frequency in the range 13.3-30 Hz is randomly chosen.
-%   7. For each spectrum, the spectral amplitude at the higher frequency 
-%       is divided by the spectral amplitude at the lower frequency. Each
+%   4. Amplitude spectra (periodograms) are computed for each 1.58-s signal waveform and the noise waveform (a more stable method for computing amplitude spectra could be useful).
+%   5. Optionally smooth the (signal and noise) amplitude spectra (Heather did not use this)
+%   6. A lower frequency band centered on 5.0 Hz is used by default, with 20% bounds, i.e. [4.0-6.0] Hz.
+%   7. A highere frequency band centered on 20.0 Hz is used by default, with 10% bounds, i.e. [18.0-22.0] Hz.
+%   8. For each spectrum, the mean spectral amplitude at the higher frequency 
+%       is divided by the mean spectral amplitude at the lower frequency. Each
 %       spectral ratio is saved.
-%   8. The natural logarithm of each spectral ratio is plotted against the
+%   9. The natural logarithm of each spectral ratio is plotted against the
 %       corresponding travel time.
-%   9. A linear regression line is fit to the spectral ratio versus travel
-%       time graph. 
-%   10. The slope of this line gives an estimate of Q.
-%   11. Steps 5-10 are repeated 10 times for different choices of
+%   10. A linear regression line is fit to the set of spectral ratios versus travel
+%       time graph (using polyfit). 
+%   11. The slope of this line gives an estimate of Q (using polyval).
+%
+%   Since spectral amplitude ratios plots versus travel time can have a lot of scatter,
+%   regression 
+%   12. Optionally, steps 5-10 are repeated 10 times for different choices of
 %       lower and higher frequency, generating more estimates of Q.
-%   12. The distribution of Q estimates is plotted, and the corresponding
+%   13. The distribution of Q estimates is plotted, and the corresponding
 %       average Q and uncertainty are estimated.
 %
 % Default parameters can be overriden with name-value pairs, e.g.
@@ -42,29 +59,55 @@ function arrivalobj = frankelq(dbpath, varargin)
 %  Additional filtering could be performed for a circular radius, a depth
 %  range, and/or a time range.
 %
-%  FRANKELQ(dbpath, 'pretrigger', 1, 'posttrigger', 0.5) sets the
-%  pretrigger window to 1-s and the posttrigger window to 0.5-s.
+%  FRANKELQ(dbpath, 'pretrigger', 0.3, 'posttrigger', 1.28) sets the
+%  pretrigger window to 0.3-s and the posttrigger window to 1.28-s.
+%  These are the default values used by Heather, because they were used by
+%  Frankel. But given that Frankel used regional rather than local earthquakes,
+%  as Heather did, these defaults should be examined.
 %
-%  FRANKELQ(dbpath, 'f1', [3.0 7.0], 'f2', [13.0 30.0]) sets the lower
+%  FRANKELQ(dbpath, 'f1', [4.0 6.0], 'f2', [18.0 22.0]) sets the lower
 %  frequency range to 3-7 Hz and the upper frequency range to 13-30 Hz.
+%  Default values are f1 = [4.0 6.0] and f2 = [18.0 22.0], to match
+%  Frankel paper.
+%  By default, these full bands are used for computing spectral ratios.
+%  However, if numFreqTrials is modified from default value of 0, a
+%  center frequency from each range is randomly chosen for each trial.
+%  See 'numFreqTrials' option below for more details.
 %
-%  FRANKELQ(dbpath, 'max_arrivals', 10) sets the maximum number of arrivals
-%  to load to 10 (Default: 999).
+%  FRANKELQ(dbpath, 'seaz', [150 210]) adds an azimuthal filter from
+%  150 to 210 degrees. By default this filter is turned off. Note that
+%  this filter could also be implemented by subsetting the database beforehandd
+%  with Antelope commands, or by passing a dbeval expression (see "eval" name-value
+%  pair above). *** SCAFFOLD: NOT IMPLEMENTED YET ***
 %
-%  FRANKELQ(dbpath, 'num_iterations', 20) sets the number of iterations,
-%  i.e. the number of different frequency pairs chosen, to 20. Default: 10
+%  FRANKELQ(dbpath, 'max_arrivals', 100) sets the maximum number of arrivals
+%  to load to 100 (Default: 999). This can be useful for testing.
+%
+%  FRANKELQ(dbpath, 'numFreqTrials', 100) sets the number of frequency trails,
+%  i.e. the number of different frequency pairs chosen, to 20. Default: 0
+%  The Default of 0 turns this off, and the full band prescribed by f1 and f2
+%  is used for computing spectral ratios. If greater than 0, for each trial, a
+%  random center frequency within the bands prescribed by f1 and f2 is chosen.
+%  The band used then extends from 10% below to 10% above. For example, if
+%  f1 = [3.0 7.0] and in a trial a center frequency of 6.0 is chosen, the actual
+%  band used will extend from 5.4 - 6.6 Hz. Similarly for f2.
+%
+%  FRANKELQ(dbpath, 'numJackKnifeTrials', 100) sets the number of trials to
+%  do jack-knifing for. By default this is set to 0, which turns jack-knifing
+%  off. If jack-knifing is ON, a random 90% of spectral ratios are used in
+%  each trial.
 %
 %  Boolean parameters:
 %
 %  FRANKELQ(dbpath, 'INTEGRATE', true) will cause waveform data to be 
 %  integrated (e.g. from velocity to displacement seismograms), prior to 
-%  computing spectra. Default: false.
+%  computing spectra. Default: false. (Note: by default Heather integrated).
 %
 %  FRANKELQ(dbpath, 'SMOOTH', true) will cause amplitude spectra to be 
 %  smoothed, prior to computing spectral ratios, in the "Compute spectra" 
 %  step. By default, spectra are not smoothed, but this can lead to less
 %  stable spectral ratios, and therefore more scatter in plots of spectral
-%  ratio vs. travel time. Default: true
+%  ratio vs. travel time. Default: true. (Note: Heather did not smooth).
 %
 %  FRANKELQ(dbpath, 'PLOT', true) will cause waveform data and spectra to 
 %  be plotted in the "Compute spectra" step, and spectral ratio vs. travel
@@ -78,8 +121,8 @@ function arrivalobj = frankelq(dbpath, varargin)
 %       dbeval_expr = 'sta==''PLWB'' && iphase==''P'''
 %       FRANKELQ(dbpath, 'expr', dbeval_expr, 'f1', [3 7], 'f2' [13 30], ...
 %           'pretrigger', 0.3, 'posttrigger', 1.28, 'max_arrivals', 100, ...
-%           'num_iterations', 30, 'PLOT', true, 'SMOOTH', true, ...
-%           'INTEGRATE', TRUE);
+%           'numFreqTrials', 30, 'numJackKnifeTrials', 50, 'INTEGRATE', true, 'SMOOTH', true, ...
+%           'PLOT', true, 'seaz', [150 210]);
 
 %   History:
 %     April 2014: Glenn Thompson
@@ -96,11 +139,14 @@ function arrivalobj = frankelq(dbpath, varargin)
 %       Added units to Y-axis on plots
 %       Added amplitude spectrum of noise before waveform onto amplitude spectrum plot
 %       Tried to implement t* method (reference needed)
+%     August 2022: Glenn Thompson
+%       Re-organized code to use functions, for easy maintainability
+%       Added options to compute Q uncertainty by trials (iterations) over randomly-selected 
+%           frequency pairs, and jack-knifing
+%     March 2023: Glenn Thompson
+%       Fixed jack-knifing to use 90% rather than 50% of samples in each trial
+%       Added option to filter based on a range of seaz
 %
-% To do:
-%   * Estimate Q and uncertainty in Q by iterating over a range of lower
-%       and upper frequency values.
-%   * Implement azimuthal dependence. Filter using seaz parameter?
 
     close all, clc
     
@@ -112,10 +158,10 @@ function arrivalobj = frankelq(dbpath, varargin)
     % Parse input variables
     p = inputParser;
     p.addParameter('expr', '');
-    p.addParameter('f1', [3.3 7.5]);
-    p.addParameter('f2', [13.3 30]);
-    p.addParameter('pretrigger', 0.5);
-    p.addParameter('posttrigger', 1.5);
+    p.addParameter('f1', [4.0 6.0]);
+    p.addParameter('f2', [18.0 22.0]);
+    p.addParameter('pretrigger', 0.3);
+    p.addParameter('posttrigger', 1.28);
     p.addParameter('max_arrivals', 999);
     p.addParameter('PLOT', true);
     p.addParameter('INTEGRATE', false);
@@ -302,13 +348,17 @@ function estimate_Q()
         end   
         
         % Compute spectral ratio based on multiple random choices of f_low and f_high
-        numFreqTrials=100;
         allQ = [];
         for freqTrialNum=1:numFreqTrials
             thisFreqQ = [];
             spectral_ratios = [];
-            f_low = f1(1) + (f1(2)-f1(1)).*rand(1,1);
-            f_high = f2(1) + (f2(2)-f2(1)).*rand(1,1);
+            if numFreqTrials>1:
+                f_low = f1(1) + (f1(2)-f1(1)).*rand(1,1);
+                f_high = f2(1) + (f2(2)-f2(1)).*rand(1,1);
+            else
+                f_low = mean(f1);
+                f_high = mean(f2);
+            end
             for i=1:numel(arrivalobj2.time)
                 s = spectra(i*2-1);
                 s_noise = spectra(i*2);
@@ -326,23 +376,32 @@ function estimate_Q()
             end     
             
             % linear regression and Q estimate 
-            numJackKnifeTrails = 100;
             numArrivals = length(arrivalobj2.time);
-            jackKnifeNumArrivals = round(numArrivals*0.5);
+            jackKnifeNumArrivals = round(numArrivals*0.9);
             for jackKnifeTrialNum=1:numJackKnifeTrails
-                % choose 50% of samples
-                indices = randperm(numArrivals);
-                indices = indices(1:jackKnifeNumArrivals);
-                [p,S] = polyfit(arrivalobj2.traveltime(indices), spectral_ratios(indices),1);
+                % LINEAR REGRESSION
+                if numJackKnifeTrials>1
+                    % choose 90% of samples
+                    indices = randperm(numArrivals);
+                    indices = indices(1:jackKnifeNumArrivals);
+                    [p,S] = polyfit(arrivalobj2.traveltime(indices), spectral_ratios(indices),1);
+                else
+                    [p,S] = polyfit(arrivalobj2.traveltime, spectral_ratios,1);
+                end
+                
+                % COMPUTE Q
                 xlim=[min(arrivalobj2.traveltime) max(arrivalobj2.traveltime)];
                 [yline,delta] = polyval(p, xlim, S);
-                delta = mean(delta); % not sure why delta has two elements
+                delta = mean(delta); % SCAFFOLD: not sure why delta has two elements
                 slope = (yline(2) - yline(1)) / (xlim(2) - xlim(1));
+                Q = -pi * (f_high - f_low) / slope;
+
+                % INITIAL WEAK ATTEMPT TO CONSTRAIN Q UNCERTAINTY
                 steepest_slope = (   (yline(2) - delta) - (yline(1)+delta) ) / (xlim(2) - xlim(1));
                 shallowest_slope = ( (yline(2) + delta) - (yline(1)-delta) ) / (xlim(2) - xlim(1));
-                Q = -pi * (f_high - f_low) / slope;
                 maxQ = -pi * (f_high - f_low) / steepest_slope;
                 minQ = -pi * (f_high - f_low) / shallowest_slope;
+
                 thisFreqQ = [thisFreqQ Q]; % Q maxQ minQ]; % weight central value twice
             
                 if PLOT
@@ -362,31 +421,35 @@ function estimate_Q()
             allQ = [allQ thisFreqQ];
             
         end
-        fprintf('Q_{%s}:\n\tmean = %.0f\n\tstdev = %.0f\n', iphase, nanmean(allQ), nanstd(allQ) )
-        fprintf('\tmedian = %.0f\n\t68%% chance in range %.0f to %.0f\n', nanmedian(allQ), prctile(allQ, 16), prctile(allQ, 84) )
-        fprintf('\t95%% chance in range %.0f to %.0f\n', prctile(allQ, 2.5), prctile(allQ, 97.5) )
-        n = length(allQ(allQ>10 & allQ<2000));
-        fprintf('\t%.0f%% of Q estimates are in range 10-2000\n', 100*n/length(allQ) ) 
 
+        if numFreqTrials>1 | numJackKnifeTrails > 1 % could replace with if numel(allQ)>1
+            % SUMMARIZE TRIALS
 
-        figure
-        plot(1:99, prctile(allQ, 1:99))
-        hold on
-        plot(50,nanmedian(allQ),'*')
-        text(50,nanmedian(allQ),'median')
-        plot([0 100],[nanmean(allQ) nanmean(allQ)])
-        text(30,nanmean(allQ),'mean')
+            fprintf('Q_{%s}:\n\tmean = %.0f\n\tstdev = %.0f\n', iphase, nanmean(allQ), nanstd(allQ) )
+            fprintf('\tmedian = %.0f\n\t68%% chance in range %.0f to %.0f\n', nanmedian(allQ), prctile(allQ, 16), prctile(allQ, 84) )
+            fprintf('\t95%% chance in range %.0f to %.0f\n', prctile(allQ, 2.5), prctile(allQ, 97.5) )
+            n = length(allQ(allQ>10 & allQ<2000));
+            fprintf('\t%.0f%% of Q estimates are in range 10-2000\n', 100*n/length(allQ) ) 
+
+            figure
+            plot(1:99, prctile(allQ, 1:99))
+            hold on
+            plot(50,nanmedian(allQ),'*')
+            text(50,nanmedian(allQ),'median')
+            plot([0 100],[nanmean(allQ) nanmean(allQ)])
+            text(30,nanmean(allQ),'mean')
                 
-        text(10,prctile(allQ,99), ...
-            sprintf('mean = %.0f\nstdev = %.0f\nmedian = %.0f\n68%% chance in range %.0f to %.0f\n95%% chance in range %.0f to %.0f\n', ...
-            nanmean(allQ), nanstd(allQ), nanmedian(allQ), ...
-            prctile(allQ, 16), prctile(allQ, 84), prctile(allQ, 2.5), ...
-            prctile(allQ, 97.5) ), ...
-            'HorizontalAlignment', 'left', ...
-            'VerticalAlignment', 'top')
-        xlabel('Percentile')
-        ylabel(sprintf('Q_{%s}',iphase))
-        title(sprintf('Distribution of Q_{%s} estimates', iphase))
+            text(10,prctile(allQ,99), ...
+                sprintf('mean = %.0f\nstdev = %.0f\nmedian = %.0f\n68%% chance in range %.0f to %.0f\n95%% chance in range %.0f to %.0f\n', ...
+                nanmean(allQ), nanstd(allQ), nanmedian(allQ), ...
+                prctile(allQ, 16), prctile(allQ, 84), prctile(allQ, 2.5), ...
+                prctile(allQ, 97.5) ), ...
+                    'HorizontalAlignment', 'left', ...
+                    'VerticalAlignment', 'top')
+            xlabel('Percentile')
+            ylabel(sprintf('Q_{%s}',iphase))
+            title(sprintf('Distribution of Q_{%s} estimates', iphase))
+        end
     end
 end
 
