45e724a1df3f15626158009ac6bfa011bef71dcf
Glenn Thompson <glennthompson1971@gmail.com>
2025-11-24 13:10:52 -0500

    remove obsolete and deprecated functionality

--- deprecated/fft_tools/+wf_fft/about.m
+++
@@ -1,7 +0,0 @@
-function about
-
-%Miscellaneous tools for working with waveforms.
-% Tools in this package are specific enough or customized enough that they
-% do not belong as built-in methods for the waveform class.
-
-help wf_fft;
