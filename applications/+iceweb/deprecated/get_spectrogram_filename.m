3b89bc8c13aa3f4a8fa4020259e75118b18eaf5c
Glenn Thompson <glennthompson1971@gmail.com>
2025-11-24 13:06:36 -0500

    trying to resolve changes

--- applications/+iceweb/deprecated/get_spectrogram_filename.m
+++
@@ -1,6 +0,0 @@
-function spectrogramFilename = get_spectrogram_filename(paths, subnet, dnum)
-debug.printfunctionstack('>');
-timestamp = datestr(dnum, 30);
-spdir = fullfile(paths.spectrogram_plots, subnet, timestamp(1:4), timestamp(5:6), timestamp(7:8));
-spectrogramFilename = fullfile(spdir, [timestamp, '.png']);
-debug.printfunctionstack('<');
--- applications/+iceweb/deprecated/show_sites.m
+++
@@ -1,5 +0,0 @@
-function show_sites(sites)
-                 for ccc=1:numel(sites)
-                     debug.print_debug(1,sites(ccc).channeltag.string())
-                 end
-end
